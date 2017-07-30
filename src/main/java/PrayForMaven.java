acf0dcd56d997ba7a41af6985d52737bcc52d8df
smiroshn <smiroshn@akamai.com>
2017-07-30 15:21:16 +0300

    enum with Browsers

--- src/main/java/PrayForMaven.java
+++ src/main/java/PrayForMaven.java
@@ -1,6 +1,4 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  * Created by Iidwuurliik on 26.07.2017.
@@ -11,20 +9,24 @@ public class PrayForMaven {
 
     public static void main(String[] args) {
 
-        System.out.println("Fuck mvn!");
         String chrome_driver_path = "C:\\Webdrivers\\chromedriver.exe";
         String firefox_driver_path = "C:\\Webdrivers\\geckodriver.exe";
 
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        Browser browser;
 
-        System.setProperty("webdriver.chrome.driver", chrome_driver_path);
-        System.setProperty("webdriver.gecko.driver",firefox_driver_path);
+        System.out.println(Browser.pickBrowser());
 
-        //driver = new ChromeDriver();
-        driver = new FirefoxDriver();
 
-        driver.navigate().to("http://google.com");
-        driver.close();
+//        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//
+//        System.setProperty("webdriver.chrome.driver", chrome_driver_path);
+//        System.setProperty("webdriver.gecko.driver", firefox_driver_path);
+//
+//        //driver = new ChromeDriver();
+//        driver = new FirefoxDriver();
+//
+//        driver.navigate().to("http://google.com");
+//        driver.close();
 
 
     }
